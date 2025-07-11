b39d2c3c022314d97046d9bfaa708b31db41f6ef
chinda <ronitchinda100@gmail.com>
2025-07-11 16:56:41 +0530

    src: Added Flux in projectdata.jsx

--- src/assets/Projectdata.jsx
+++ src/assets/Projectdata.jsx
@@ -1,5 +1,5 @@
 import upsideAvenue from "./upsideAvenue.png";
-import checkers from "./checkers.png";
+import flux from "./Flux.png";
 import NagaConnect from "./NagaConnect_Asset.png";
 
 export const Projectdata = [
@@ -11,7 +11,17 @@ export const Projectdata = [
       "NagaConnect is a platform that connects job seekers with employers in Nagaland’s unorganized and semi-organized sectors. It focuses on local roles in driving, hospitality, tourism, office work, and labor. Businesses can use the platform to hire workers, list products, and advertise, supporting both employment and regional business growth.",
     ],
     tech_stack: ["Kotlin", "Firebase", "Node.js", "Gemini API"],
-    link: "https://chindaronit.github.io" ,
+    link: "https://chindaronit.github.io",
+  },
+  {
+    img: flux,
+    title: "Flux",
+    subtitle: "Open-Source",
+    desc: [
+      "Flux is a sleek, minimalistic productivity app designed to help you stay organized with ease. Featuring a modern Material 3 UI, it offers customizable workspace templates for structuring your day, biometric app lock for added security. Flux brings a seamless, focused workflow to your fingertips, making it easier to stay in control and maintain your productivity."
+    ],
+    tech_stack: ["Kotlin", "Jetpack-Compose", "Room Database"],
+    link: "https://github.com/chindaronit/Flux",
   },
   {
     img: upsideAvenue,
@@ -22,17 +32,5 @@ export const Projectdata = [
     ],
     tech_stack: ["HTML", "CSS", "Spring Boot", "Bootstrap", "Mysql"],
     link: "https://github.com/chindaronit/upSideAvenue",
-  },
-  {
-    img: checkers,
-    title: "Standard Checkers",
-    subtitle: "Under Prof. Aparjita Dutta",
-    desc: [
-      "Enhanced gaming experience with AI opponents",
-      "Customize AI performance through game difficulty settings",
-      "Implemented Minimax Algorithm with Alpha-Beta Pruning algorithm.",
-    ],
-    tech_stack: ["Python", "Pygame"],
-    link: "https://github.com/chindaronit/Checkers-TicTacToe",
   },
 ];
